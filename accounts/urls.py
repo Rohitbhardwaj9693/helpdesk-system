91dd36696b258818d41a1099ba08f9e2f28a191e
Rohitbhardwaj9693 <Rohitbhardwaj9693@gmail.com>
2026-09-18 23:55:29 +0530

    Add user search role filter and pagination

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (LoginView, MeView,AdminTestView,
     ManagerTestView,
-    AgentTestView,)
+    AgentTestView, UserListCreateView,)
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("admin-test/",AdminTestView.as_view(),name="admin-test"),
     path("manager-test/",ManagerTestView.as_view(),name="manager-test"),
     path("agent-test/",AgentTestView.as_view(),name="agent-test"),
+    path("users/", UserListCreateView.as_view(), name="user-list-create"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
-from .serializers import LoginSerializer
 from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdmin ,IsManager ,IsAgent
+from .serializers import (
+    LoginSerializer,
+    UserSerializer,
+    CreateUserSerializer,
+)
+from .models import User
+from django.db.models import Q
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 class LoginView(APIView):
     def post(self,request):
@@ -84,3 +91,58 @@ class AgentTestView(APIView):
             {"message":"You are Agent"},
             status=status.HTTP_200_OK
         )
+class UserListCreateView(APIView):
+    permission_classes = [IsAdmin]
+
+    def get(self, request):
+        search = request.query_params.get("search","")
+        users = User.objects.all().order_by("-id")
+        role = request.query_params.get("role")
+
+        if search:
+            users = users.filter(
+                Q(full_name__icontains=search)
+                |Q(email__icontains=search)
+                |Q(phone__icontains=search)
+                |Q(department__icontains=search)
+            )
+
+
+        if role:
+         users = users.filter(role=role)
+        paginator = UserPagination()
+        page = paginator.paginate_queryset(users, request)
+
+        serializer = UserSerializer(page, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def post(self, request):
+        serializer = CreateUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(
+                {
+                    "message": "User created successfully",
+                    "user": UserSerializer(user).data
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+class UserPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "limit"
+    max_page_size = 100
+    
